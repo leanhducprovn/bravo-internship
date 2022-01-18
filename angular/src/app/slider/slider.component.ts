9804efd40d1f0d50ca67f4597641a036c82b83e7
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-01-18 08:25:49 +0700

    Commit to 135 day ago

--- angular/src/app/slider/slider.component.ts
+++ angular/src/app/slider/slider.component.ts
@@ -31,7 +31,7 @@ export class SliderComponent
   ngOnInit(): void {
     this.slider();
     this.onEvent();
-    console.log('commit về 141 ngày trước');
+    console.log('commit về 135 ngày trước');
   }
 
   start!: number;
