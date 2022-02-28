1158a87e5c762651f9abdbf14ea61a07837d5ecb
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-02-28 08:38:35 +0700

    add concat

---
+++ js-array/concat.js
@@ -0,0 +1,5 @@
+const arr1 = ["Lê Anh Đức", "Thực tập sinh"];
+const arr2 = ["Công nghệ thông tin", "Bravo"];
+const arr3 = ["Mảng FrontEnd", "Angular"];
+const concat = arr1.concat(arr2, arr3);
+document.getElementById("concat").innerHTML = concat;
