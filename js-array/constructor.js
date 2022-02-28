635ab2a447381ec228e7bfe11cf3723428f8480e
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-02-28 10:19:09 +0700

    update

--- js-array/constructor.js
+++ js-array/constructor.js
@@ -1,5 +1,6 @@
 /* --------------------------------
 - Trả về hàm đã tạo mảng nguyên mẫu
+- Syntax: array.constructor
 ---------------------------------*/
 
 const info = ["Lê Anh Đức", "Đông Anh", "Hà Nội", "Việt Nam"];
