4b5318013a8c0046a2ba4ed9f53e0d2505af6009
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-02-28 09:49:33 +0700

    add fill

---
+++ js-array/fill.js
@@ -0,0 +1,7 @@
+/* ----------------------------------------------------------------
+- Điền vào các phần tử được chỉ định trong một mảng với một giá trị
+- Syntax: array.fill(value, start, end)
+-----------------------------------------------------------------*/
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("fill").innerHTML = fruits.fill("Lê Anh Đức", 1, 3);
