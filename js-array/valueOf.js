3e605fbe177780e277b52faef9a7f2485a809b84
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-02-28 16:04:03 +0700

    add valueOf

--- js-array/valueOf.js
+++ js-array/valueOf.js
@@ -4,3 +4,7 @@
 - Syntax: array.valueOf()
 -----------------------------------------*/
 
+const arrayValueOf = ["Lê Anh Đức", "Đông Anh", "Hà Nội", "Việt Nam", "Trái đất"];
+const myArrayValueOf = arrayValueOf.valueOf();
+
+document.getElementById("valueOf").innerHTML = myArrayValueOf;
