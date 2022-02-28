a1ae70e58169343d89c2bf2f486948991bddd93d
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-02-28 15:01:27 +0700

    add reverse

---
+++ js-array/reverse.js
@@ -0,0 +1,8 @@
+/* -----------------------------------------------
+- Đảo ngược thứ tự của các phần tử trong một mảng.
+- Ghi đè lên mảng ban đầu
+- Syntax: array.reverse()
+------------------------------------------------*/
+
+const arrayReverse = ["Lê Anh Đức", "Đông Anh", "Hà Nội", "Việt Nam", "Trái đất"];
+document.getElementById("reverse").innerHTML = arrayReverse.reverse();
