4c455d120abcc3df2e29ef8297687357c0f24cbc
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-02-28 14:47:17 +0700

    add reduce

---
+++ js-array/reduce.js
@@ -0,0 +1,14 @@
+/* -----------------------------------------------------------------------------------
+- Thực thi một hàm giảm thiểu cho phần tử mảng.
+- Trả về một giá trị duy nhất: kết quả tích lũy của hàm.
+- Không thay đổi mảng ban đầu
+- Syntax: array.reduce(function(total, currentValue, currentIndex, arr), initialValue)
+-------------------------------------------------------------------------------------*/
+
+const arrayReduce = [175, 50, 25];
+document.getElementById("reduce").innerHTML = arrayReduce.reduce(myFunc);
+
+// Trừ các số trong mảng, bắt đầu từ bên trái:
+function myFunc(total, num) {
+    return total - num;
+}
