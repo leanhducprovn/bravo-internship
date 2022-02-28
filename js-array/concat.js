319a717d6d3758b08f41d2ae563560815d5e25f0
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-02-28 08:44:02 +0700

    update concat

--- js-array/concat.js
+++ js-array/concat.js
@@ -1,3 +1,9 @@
+/* -----------------------------------------
+- Nối 2 hoặc nhiều mảng với nhau.
+- Trả về một mảng mới, chứa các mảng đã nối.
+- Không thay đổi các mảng hiện có.
+-------------------------------------------*/
+
 const arr1 = ["Lê Anh Đức", "Thực tập sinh"];
 const arr2 = ["Công nghệ thông tin", "Bravo"];
 const arr3 = ["Mảng FrontEnd", "Angular"];
