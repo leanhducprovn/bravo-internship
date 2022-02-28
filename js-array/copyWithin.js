7d9b938ea5cc5951c416dacc63fa336db5721fe0
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-02-28 10:16:27 +0700

    update copyWithin

--- js-array/copyWithin.js
+++ js-array/copyWithin.js
@@ -1,8 +1,10 @@
 /* ------------------------------------------------------------
 - Sao chép các phần tử mảng sang một vị trí khác trong một mảng
 - Ghi đè các giá trị hiện có
+- Syntax: array.copyWithin(target, start, end)
 -------------------------------------------------------------*/
 
-const number = ["1", "2", "3", "4"];
-copyWithin = number.copyWithin(0, 3);
+const number = ["1", "2", "3", "4", "5"];
+// Sao chép hai phần tử mảng đầu tiên vào vị trí thứ ba và thứ tư:
+copyWithin = number.copyWithin(2, 0, 2);
 document.getElementById("copyWithin").innerHTML = copyWithin;
