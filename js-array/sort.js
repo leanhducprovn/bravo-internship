a8af172a926fbe21253d3c12264de82d988f45cb
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-02-28 15:46:43 +0700

    update

---
+++ js-array/sort.js
@@ -0,0 +1,9 @@
+/* ------------------------------------------------------------------------
+- Sắp xếp các phần tử của một mảng
+- Ghi đè mảng ban đầu
+- Sắp xếp các phần tử dưới dạng chuỗi theo thứ tự bảng chữ cái và tăng dần.
+- Syntax: array.sort(compareFunction)
+-------------------------------------------------------------------------*/
+
+const arraySort = ["Lê Anh Đức", "Đông Anh", "Hà Nội", "Việt Nam", "Trái đất"];
+document.getElementById("sort").innerHTML = arraySort.sort();
---
+++ js-array/splice.js
@@ -0,0 +1,14 @@
+/* --------------------------------------------------------
+- Thêm hoặc loại bỏ các phần tử mảng.
+- Ghi đè mảng ban đầu
+- Syntax: array.splice(index, howmany, item1, ....., itemX)
+----------------------------------------------------------*/
+
+const arraySplice = ["Lê Anh Đức", "Đông Anh", "Hà Nội", "Việt Nam", "Trái đất"];
+
+// Tại vị trí 2, thêm 2 phần tử:
+arraySplice.splice(3, 0, "Thực tập sinh", "Lập trình web");
+// Tại vị trí 2, xóa 2 mục:
+// fruits.splice(2, 2);
+
+document.getElementById("splice").innerHTML = arraySplice;
