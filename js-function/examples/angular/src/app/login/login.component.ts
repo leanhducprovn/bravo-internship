e70472152ce27fd97255db82f05a108be60b8a5a
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-03-17 15:31:38 +0700

    complete

--- js-function/examples/angular/src/app/login/login.component.ts
+++ js-function/examples/angular/src/app/login/login.component.ts
@@ -6,19 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  user = {
-    username: 'admin',
-    password: 'admin',
-  };
-
-  login(event: MouseEvent) {}
-
-  constructor() {
-    const btn = document.getElementById('button');
-    if (btn != null) {
-      btn.addEventListener('click', this.login);
+  login() {
+    const setUsername = 'admin';
+    const setPassword = 'admin';
+    const done = setUsername + setPassword;
+    const username = document.getElementById('username') as HTMLInputElement;
+    const password = document.getElementById('password') as HTMLInputElement;
+    if (username != null && password != null) {
+      const checking = username.value + password.value;
+      if (checking == done) {
+        const wrapper = document.querySelector('.codepro-login-wrapper');
+        if (wrapper != null) {
+          wrapper.classList.add('hidden');
+        }
+      } else {
+        const wrong = document.getElementById('codepro-login-check');
+        if (wrong != null) {
+          wrong.innerHTML = 'wrong username or password!';
+        }
+      }
     }
-    console.log();
   }
+
+  click(event: MouseEvent) {
+    if (event) {
+      const btn = document.getElementById('button');
+      if (btn != null) {
+        btn.addEventListener('mouseout', this.login);
+      }
+    }
+  }
+
+  constructor() {}
   ngOnInit(): void {}
 }
