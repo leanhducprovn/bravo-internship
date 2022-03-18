c91f812d97df796c6aa7b9131846744bb065ab60
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-03-18 11:13:53 +0700

    update

---
+++ angular/components/lifecycle-hooks/src/app/example/example.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-example',
+  templateUrl: './example.component.html',
+  styleUrls: ['./example.component.css'],
+})
+export class ExampleComponent implements OnInit {
+  constructor() {}
+
+  ngOnChanges(): void {}
+
+  ngOnInit(): void {}
+
+  ngDoCheck(): void {}
+
+  ngAfterContentInit(): void {}
+
+  ngAfterContentChecked(): void {}
+
+  ngAfterViewInit(): void {}
+
+  ngAfterViewChecked(): void {}
+
+  ngOnDestroy(): void {}
+}
