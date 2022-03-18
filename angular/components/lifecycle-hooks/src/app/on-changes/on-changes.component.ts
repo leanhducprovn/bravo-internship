ebb20ea6930f6e50d283fbcdac851b40b01044ea
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-03-18 11:56:12 +0700

    update

--- angular/components/lifecycle-hooks/src/app/on-changes/on-changes.component.ts
+++ angular/components/lifecycle-hooks/src/app/on-changes/on-changes.component.ts
@@ -1,12 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
+import { Hero } from '../Hero/hero';
 @Component({
   selector: 'app-on-changes',
   templateUrl: './on-changes.component.html',
   styleUrls: ['./on-changes.component.css'],
 })
-export class OnChangesComponent implements OnInit {
-  constructor() {}
+export class OnChangesComponent implements OnChanges {
+  @Input() hero!: Hero;
+  @Input() power = '';
 
-  ngOnInit(): void {}
+  changeLog: string[] = [];
+
+  ngOnChanges(changes: SimpleChanges) {
+    for (const propName in changes) {
+      const chng = changes[propName];
+      const cur = JSON.stringify(chng.currentValue);
+      const prev = JSON.stringify(chng.previousValue);
+      this.changeLog.push(
+        `${propName}: currentValue = ${cur}, previousValue = ${prev}`
+      );
+    }
+  }
+
+  reset() {
+    this.changeLog = [];
+  }
 }
