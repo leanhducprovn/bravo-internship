87a1892bda00e801b1e10d49108c4400a41d8ac8
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-03-18 11:27:50 +0700

    delete

--- angular/components/lifecycle-hooks/src/app/app.module.ts
+++ angular/components/lifecycle-hooks/src/app/app.module.ts
@@ -3,18 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ExampleComponent } from './example/example.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ExampleComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
