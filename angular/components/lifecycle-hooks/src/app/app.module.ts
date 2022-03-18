547f2a9214143dd6dbeb4a48623b8bcefcec4a85
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-03-18 11:33:31 +0700

    create on changes

--- angular/components/lifecycle-hooks/src/app/app.module.ts
+++ angular/components/lifecycle-hooks/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OnChangesComponent } from './on-changes/on-changes.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, OnChangesComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
