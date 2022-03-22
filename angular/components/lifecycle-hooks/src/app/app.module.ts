b9e79b84dd1edd0fd3e543e57c8432f1dd796395
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-03-22 10:18:26 +0700

    create child, view-child

--- angular/components/lifecycle-hooks/src/app/app.module.ts
+++ angular/components/lifecycle-hooks/src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OnChangesComponent } from './on-changes/on-changes.component';
 import { OnChangesParentComponent } from './on-changes-parent/on-changes-parent.component';
+import { ChildComponent } from './child/child.component';
+import { ChildViewComponent } from './child-view/child-view.component';
 
 @NgModule({
-  declarations: [AppComponent, OnChangesComponent, OnChangesParentComponent],
+  declarations: [AppComponent, OnChangesComponent, OnChangesParentComponent, ChildComponent, ChildViewComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
