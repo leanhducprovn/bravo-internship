8c114a8ffdad5d6ed3781b028b647f241b540c44
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-03-22 13:19:32 +0700

    update

--- angular/components/lifecycle-hooks/src/app/oninit/oninit.component.ts
+++ angular/components/lifecycle-hooks/src/app/oninit/oninit.component.ts
@@ -7,8 +7,13 @@ import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 })
 export class OninitComponent implements OnChanges, OnInit {
   title = 'Oninit';
+  name = 'Lê Anh Đức';
+  address = 'Đông Anh, Hà Nội';
+  info = this.name + ' → ' + this.address;
   constructor() {}
   ngOnChanges(changes: SimpleChanges): void {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log(this.info);
+  }
 }
