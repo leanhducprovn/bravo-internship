124f0d6522b6a12af52cc363f28d25d1ccc4a9c3
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-03-22 08:45:45 +0700

    update

--- angular/components/lifecycle-hooks/src/app/app.module.ts
+++ angular/components/lifecycle-hooks/src/app/app.module.ts
@@ -4,10 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OnChangesComponent } from './on-changes/on-changes.component';
-import { OnChangesParentComponent } from './on-changes-parent/on-changes-parent.component';
 
 @NgModule({
-  declarations: [AppComponent, OnChangesComponent, OnChangesParentComponent],
+  declarations: [AppComponent, OnChangesComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
