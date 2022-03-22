05447d5536e512157767634b4f3aca74db56aa1b
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-03-22 11:48:42 +0700

    update

--- angular/components/lifecycle-hooks/src/app/app.module.ts
+++ angular/components/lifecycle-hooks/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { OnChangesComponent } from './on-changes/on-changes.component';
 import { OnChangesParentComponent } from './on-changes-parent/on-changes-parent.component';
 import { ChildComponent } from './child/child.component';
 import { ChildViewComponent } from './child-view/child-view.component';
+import { OninitComponent } from './oninit/oninit.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { ChildViewComponent } from './child-view/child-view.component';
     OnChangesParentComponent,
     ChildComponent,
     ChildViewComponent,
+    OninitComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
---
+++ angular/components/lifecycle-hooks/src/app/oninit/oninit.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-oninit',
+  templateUrl: './oninit.component.html',
+  styleUrls: ['./oninit.component.css'],
+})
+export class OninitComponent implements OnChanges, OnInit {
+  title = 'Oninit';
+  constructor() {
+    var name = 'Lê Anh Đức';
+  }
+  ngOnChanges(changes: SimpleChanges): void {}
+
+  ngOnInit(): void {
+    console.log(OninitComponent.constructor());
+  }
+}
