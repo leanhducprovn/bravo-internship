822dfd4b649ee30998e4eb8503e80bb865d9b530
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-03-24 08:44:42 +0700

    update

--- angular/components/lifecycle-hooks/src/app/app-routing.module.ts
+++ angular/components/lifecycle-hooks/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DoCheckParentComponent } from './do-check-parent/do-check-parent.component';
+import { HomeComponent } from './home/home.component';
 import { LifecycleHooksComponent } from './lifecycle-hooks/lifecycle-hooks.component';
 import { OnChangesParentComponent } from './on-changes-parent/on-changes-parent.component';
 import { OninitComponent } from './oninit/oninit.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'lifecycle-hooks', component: LifecycleHooksComponent },
   { path: 'lifecycle-hooks/on-changes', component: OnChangesParentComponent },
   { path: 'lifecycle-hooks/do-check', component: DoCheckParentComponent },
