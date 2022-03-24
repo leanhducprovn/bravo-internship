d5c6949014bcb0ea2e27ac3130f5324c7e9c119e
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-03-24 13:14:37 +0700

    update

--- angular/components/lifecycle-hooks/src/app/after-content-init-parent/after-content-init-parent.component.ts
+++ angular/components/lifecycle-hooks/src/app/after-content-init-parent/after-content-init-parent.component.ts
@@ -3,26 +3,8 @@ import { Component } from '@angular/core';
 import { LoggerService } from '../logger.service';
 @Component({
   selector: 'app-after-content-init-parent',
-  // templateUrl: './after-content-init-parent.component.html',
+  templateUrl: './after-content-init-parent.component.html',
   styleUrls: ['./after-content-init-parent.component.css'],
-  template:
-    `
-  <div class="parent">
-    <h2>AfterContent</h2>
-
-    <div *ngIf="show">` +
-    `<after-content>
-        <app-child></app-child>
-      </after-content>` +
-    `</div>
-
-    <div class="info">
-      <h3>AfterContent Logs</h3>
-      <button type="button" (click)="reset()">Reset</button>
-      <div *ngFor="let msg of logger.logs" class="log">{{msg}}</div>
-    </div>
-  </div>
-  `,
   providers: [LoggerService],
 })
 export class AfterContentInitParentComponent {
