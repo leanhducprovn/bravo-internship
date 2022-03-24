e34aba1b33c6c8e4716cfc38dec3cf5a0927e73f
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-03-24 16:58:38 +0700

    update

--- angular/src/app/lifecycle-hooks/ondestroy/ondestroy.component.ts
+++ angular/src/app/lifecycle-hooks/ondestroy/ondestroy.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-ondestroy',
   templateUrl: './ondestroy.component.html',
-  styleUrls: ['./ondestroy.component.css']
+  styleUrls: ['./ondestroy.component.css'],
 })
-export class OndestroyComponent implements OnInit {
-
-  constructor() { }
+export class OndestroyComponent implements OnInit, OnDestroy {
+  constructor() {}
 
   ngOnInit(): void {
+    console.warn('Destroy test');
   }
 
+  ngOnDestroy(): void {
+    console.error(this.ngOnInit());
+  }
 }
