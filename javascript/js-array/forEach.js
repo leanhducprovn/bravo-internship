28c8e336debc0f29815c2f628ce0660b559392e6
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-03-25 11:56:47 +0700

    update

--- javascript/js-array/forEach.js
+++ javascript/js-array/forEach.js
@@ -15,32 +15,46 @@ function myFunction(item, index) {
 }
 
 let index1 = 0;
+console.log("while");
 while (index1 < arrayForEach.length) {
 	console.warn(arrayForEach[index1]);
 	index1++;
 }
 
 let index2 = 0;
+console.log("for");
 for (index2; index2 < arrayForEach.length; index2++) {
 	console.error(arrayForEach[index2]);
 }
 
 let index3 = 0;
+console.log("map");
 arrayForEach.map(function (item) {
 	index3 = item;
 	console.warn(index3);
 });
 
 let index4 = 0;
+console.log("do... while");
 do {
 	console.error(arrayForEach[index4]);
 	index4++;
 } while (index4 < arrayForEach.length);
 
+console.log("for... of");
 for (var index5 of arrayForEach) {
 	console.warn(index5);
 }
 
+console.log("for... in");
 for (var index6 in arrayForEach) {
 	console.error(arrayForEach[index6]);
 }
+
+console.log("forEach breack");
+arrayForEach.forEach(function (item) {
+	console.warn(item);
+	if (item == "Phạm Văn Linh") {
+		breack;
+	}
+});
