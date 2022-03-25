d976af52d1da0825485378aa8759205f09cdadf7
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-03-25 11:23:25 +0700

    update

--- javascript/js-array/forEach.js
+++ javascript/js-array/forEach.js
@@ -14,11 +14,32 @@ function myFunction(item, index) {
 	text += index + ": " + item + " | ";
 }
 
-while ((index1 = 0 < arrayForEach.length)) {
+let index1 = 0;
+while (index1 < arrayForEach.length) {
 	console.warn(arrayForEach[index1]);
 	index1++;
 }
 
-for (let index2 = 0; index2 < arrayForEach.length; index2++) {
+let index2 = 0;
+for (index2; index2 < arrayForEach.length; index2++) {
 	console.error(arrayForEach[index2]);
+}
+
+let index3 = arrayForEach.map((arr) => arr);
+for (var i = 0; i < arrayForEach.length; i++) {
+	console.warn(index3[i]);
+}
+
+let index4 = 0;
+do {
+	console.error(arrayForEach[index4]);
+	index4++;
+} while (index4 < arrayForEach.length);
+
+for (var index5 of arrayForEach) {
+	console.warn(index5);
+}
+
+for (var index6 in arrayForEach) {
+	console.error(arrayForEach[index6]);
 }
