b32c2f2af882118dccf82e8a06102e1000429016
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-03-25 16:27:36 +0700

    update

--- angular/src/app/custom-form/form/form.component.ts
+++ angular/src/app/custom-form/form/form.component.ts
@@ -7,6 +7,11 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./form.component.css'],
 })
 export class FormComponent implements OnInit {
+  user = {
+    username: 'admin',
+    password: 'admin',
+  };
+
   checkLogin = this.fb.group({
     Username: ['', [Validators.required]],
     Password: ['', [Validators.required]],
@@ -19,8 +24,22 @@ export class FormComponent implements OnInit {
     return this.checkLogin.get('Password')!;
   }
 
+  done() {
+    return this.user.username + this.user.password;
+  }
+
   login() {
     console.warn(this.checkLogin.value);
+    const checking =
+      this.checkLogin.value.Username + this.checkLogin.value.Password;
+    if (checking == this.done()) {
+      return window.location.replace('/');
+    } else {
+      const wrong = document.getElementById('codepro-login-check');
+      if (wrong != null) {
+        wrong.innerHTML = 'wrong username or password!';
+      }
+    }
   }
 
   constructor(private fb: FormBuilder) {}
