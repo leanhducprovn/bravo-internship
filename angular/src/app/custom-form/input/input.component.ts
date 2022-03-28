7d210294b49eccd118170b897eddb952fcfcc730
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-03-28 10:17:35 +0700

    update

--- angular/src/app/custom-form/input/input.component.ts
+++ angular/src/app/custom-form/input/input.component.ts
@@ -1,12 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true,
+    },
+  ],
 })
-export class InputComponent implements OnInit {
+export class InputComponent implements OnInit, ControlValueAccessor {
+  @Input() username!: string;
+  @Input() passwork!: string;
+
   constructor() {}
+
+  typing() {
+    if (this.username != '' && this.passwork != '') {
+      console.log('test');
+    }
+  }
+
+  writeValue(obj: any): void {}
+  registerOnChange(fn: any): void {}
+  registerOnTouched(fn: any): void {}
 
   ngOnInit(): void {}
 }
