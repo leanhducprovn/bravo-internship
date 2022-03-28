b8a8701564eda4f2a8c6b2a926681558f386b8f9
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-03-28 15:05:27 +0700

    update

--- angular/src/app/custom-form/input/input.component.ts
+++ angular/src/app/custom-form/input/input.component.ts
@@ -1,6 +1,5 @@
-import { Component, Input, OnInit, forwardRef } from '@angular/core';
+import { Component, OnInit, forwardRef } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
-
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
@@ -8,21 +7,55 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
   providers: [
     {
       provide: NG_VALUE_ACCESSOR,
-      useExisting: forwardRef(() => InputComponent),
       multi: true,
+      useExisting: InputComponent,
     },
   ],
 })
 export class InputComponent implements OnInit, ControlValueAccessor {
+  typing: any;
+
+  onChange = (typing: any) => {};
+
+  onTouched = () => {};
+
+  touched = false;
+
   constructor() {}
 
-  typing() {
-    console.log('test');
+  checkUsername() {
+    this.markAsTouched();
+    this.onChange(this.typing);
   }
 
-  writeValue(obj: any): void {}
-  registerOnChange(fn: any): void {}
-  registerOnTouched(fn: any): void {}
+  checkPasswork() {
+    this.markAsTouched();
+    this.onChange(this.typing);
+  }
 
-  ngOnInit(): void {}
+  markAsTouched() {
+    if (!this.touched) {
+      this.onTouched();
+      this.touched = true;
+    }
+  }
+
+  writeValue(typing: any) {
+    this.typing = typing;
+  }
+  registerOnChange(onChange: any) {
+    this.onChange = onChange;
+  }
+
+  registerOnTouched(onTouched: any) {
+    this.onTouched = onTouched;
+  }
+
+  ngOnInit(): void {
+    const input = document.getElementById('input');
+    if (input != null) {
+      input.inputMode = 'text';
+      input.innerText = 'admin';
+    }
+  }
 }
