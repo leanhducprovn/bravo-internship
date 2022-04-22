56107847fa647af03f3ea7fb064e8492ddf10984
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-04-22 10:10:23 +0700

    update

--- angular/src/app/wijmo/input-calendar/input-calendar.component.ts
+++ angular/src/app/wijmo/input-calendar/input-calendar.component.ts
@@ -6,7 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input-calendar.component.css'],
 })
 export class InputCalendarComponent implements OnInit {
-  constructor() {}
+  firstDay!: Date;
+  lastDay!: Date;
+
+  constructor() {
+    let curr = new Date();
+    // console.log(curr);
+    // console.log(curr.getDate());
+    // console.log(curr.getDay());
+    this.firstDay = new Date(curr.setDate(curr.getDate() - curr.getDay()));
+    // console.log(this.firstDay);
+    this.lastDay = new Date(curr.setDate(curr.getDate() - curr.getDay() + 6));
+    // console.log(this.lastDay);
+  }
 
   ngOnInit(): void {}
 }
