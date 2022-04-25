c1072367de286154a799fd5c744ff8b98c92d56a
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-04-25 13:55:23 +0700

    update

--- angular/src/app/core/content-child-parent/content-child-parent.component.ts
+++ angular/src/app/core/content-child-parent/content-child-parent.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-content-child-parent',
   templateUrl: './content-child-parent.component.html',
-  styleUrls: ['./content-child-parent.component.css']
+  styleUrls: ['./content-child-parent.component.css'],
 })
-export class ContentChildParentComponent implements OnInit {
+export class ContentChildParentComponent implements OnInit, DoCheck {
+  output!: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngDoCheck() {
+    let input = document.getElementById('input') as HTMLInputElement;
+    if (input != null) {
+      this.output = input.value.toLocaleUpperCase();
+    }
   }
 
+  ngOnInit(): void {}
 }
--- angular/src/app/core/content-child/content-child.component.ts
+++ angular/src/app/core/content-child/content-child.component.ts
@@ -1,13 +1,15 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, DoCheck, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-content-child',
   templateUrl: './content-child.component.html',
   styleUrls: ['./content-child.component.css'],
 })
-export class ContentChildComponent implements OnInit {
+export class ContentChildComponent implements OnInit, DoCheck {
   @Input() label!: string;
   @Input() value!: string;
+
+  ngDoCheck() {}
 
   constructor() {}
 
