8d763ed79ebe67e48ebcfc3d2c02d698f00ae83e
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-04-25 17:02:38 +0700

    update

--- angular/src/app/wijmo/input-calendar-app/input-calendar-app-selection/input-calendar-app-selection.component.ts
+++ angular/src/app/wijmo/input-calendar-app/input-calendar-app-selection/input-calendar-app-selection.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-input-calendar-app-selection',
   templateUrl: './input-calendar-app-selection.component.html',
-  styleUrls: ['./input-calendar-app-selection.component.css']
+  styleUrls: ['./input-calendar-app-selection.component.css'],
 })
 export class InputCalendarAppSelectionComponent implements OnInit {
+  @Input() chooseTime!: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
