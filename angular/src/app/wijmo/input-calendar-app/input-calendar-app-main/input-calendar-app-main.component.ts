ca2a9a6b014cf29a171bb883bc063d2c99d1c150
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-04-25 16:41:36 +0700

    update

--- angular/src/app/wijmo/input-calendar-app/input-calendar-app-main/input-calendar-app-main.component.ts
+++ angular/src/app/wijmo/input-calendar-app/input-calendar-app-main/input-calendar-app-main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 import { Data } from '@angular/router';
 
 @Component({
@@ -6,8 +6,21 @@ import { Data } from '@angular/router';
   templateUrl: './input-calendar-app-main.component.html',
   styleUrls: ['./input-calendar-app-main.component.css'],
 })
-export class InputCalendarAppMainComponent implements OnInit {
+export class InputCalendarAppMainComponent implements OnInit, DoCheck {
   constructor() {}
+
+  ngDoCheck() {
+    const setHeader = document.getElementsByClassName('wj-header');
+    setHeader[0].innerHTML = `
+      <td>CN</td>
+      <td>T2</td>
+      <td>T3</td>
+      <td>T4</td>
+      <td>T5</td>
+      <td>T6</td>
+      <td>T7</td>
+    `;
+  }
 
   ngOnInit(): void {}
 }
