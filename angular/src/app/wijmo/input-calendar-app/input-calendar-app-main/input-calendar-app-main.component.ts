3bdcc3f1a1bcc85dcec5f0d6c1a9cd92b615008b
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-05-04 11:47:21 +0700

    update

--- angular/src/app/wijmo/input-calendar-app/input-calendar-app-main/input-calendar-app-main.component.ts
+++ angular/src/app/wijmo/input-calendar-app/input-calendar-app-main/input-calendar-app-main.component.ts
@@ -133,6 +133,22 @@ export class InputCalendarAppMainComponent
   }
 
   onThisWeek() {
+    if (this.check == false) {
+      let data = new Date();
+      data.setMonth(data.getMonth());
+      this.calendarApp.value = data;
+      this.calendarApp.itemFormatter = (date, element) => {
+        if (this.getWeek(date) == this.getWeek(new Date())) {
+          element.style.backgroundColor = '#c3e4ff';
+        }
+      };
+      this.check = true;
+    } else {
+      this.calendarApp.itemFormatter = (date, element) => {
+        element.style.backgroundColor = '';
+      };
+      this.check = false;
+    }
     // let current = new Date();
     // let dayCurrent = current.getDay();
     // let dateCurrent = current.getDate();
@@ -263,6 +279,17 @@ export class InputCalendarAppMainComponent
       default:
         return 30;
     }
+  }
+
+  getWeek(time: any) {
+    let dayNr = (time.getDay() + 6) % 7;
+    let firstThursday = time.valueOf();
+    time.setDate(time.getDate() - dayNr + 3);
+    time.setMonth(0, 1);
+    if (time.getDay() != 4) {
+      time.setMonth(0, 1 + ((4 - time.getDay() + 7) % 7));
+    }
+    return 1 + Math.ceil((firstThursday - time) / 604800000);
   }
 
   ngOnInit(): void {}
