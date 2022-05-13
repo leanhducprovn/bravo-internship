dc116341aa20feb9bada1d5f62ada7ed9c876abd
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-05-13 11:18:28 +0700

    update

--- angular/src/app/css/css-flexbox/css-flexbox.component.ts
+++ angular/src/app/css/css-flexbox/css-flexbox.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-css-flexbox',
   templateUrl: './css-flexbox.component.html',
-  styleUrls: ['./css-flexbox.component.css']
+  styleUrls: ['./css-flexbox.component.css'],
 })
 export class CssFlexboxComponent implements OnInit {
+  currentTime = new Date();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
