051603bbc54e7998721bd2fa66e48120e89eace5
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-05-17 14:51:04 +0700

    update layout

--- angular/src/app/wijmo/data-grid/data-grid.component.ts
+++ angular/src/app/wijmo/data-grid/data-grid.component.ts
@@ -21,7 +21,7 @@ export class DataGridComponent implements OnInit {
   private getData() {
     let data = product;
     return new CollectionView(data, {
-      pageSize: 20,
+      pageSize: 10,
     });
   }
 
