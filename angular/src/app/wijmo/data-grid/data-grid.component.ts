8a3043611a7584d4c5c18a1cf44ba908ca52fd9f
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-05-17 11:25:35 +0700

    update

--- angular/src/app/wijmo/data-grid/data-grid.component.ts
+++ angular/src/app/wijmo/data-grid/data-grid.component.ts
@@ -32,6 +32,30 @@ export class DataGridComponent implements OnInit {
     });
   }
 
+  onloadedRows(flexGrid: wjcGrid.FlexGrid) {
+    for (var i = 0; i < flexGrid.rows.length; i++) {
+      var row = flexGrid.rows[i];
+      var item = row.dataItem;
+      if (item.Unit == 'Bảng') {
+        row.cssClass = 'unit-bang';
+      } else if (item.Unit == 'Chiếc') {
+        row.cssClass = 'unit-chiec';
+      } else if (item.Unit == 'Chuyến') {
+        row.cssClass = 'unit-chuyen';
+      } else if (item.Unit == 'Cái') {
+        row.cssClass = 'unit-cai';
+      } else if (item.Unit == 'PCS') {
+        row.cssClass = 'unit-pcs';
+      } else if (item.Unit == 'Bộ') {
+        row.cssClass = 'unit-bo';
+      } else if (item.Unit == 'Lọ') {
+        row.cssClass = 'unit-lo';
+      } else if (item.Unit == 'Chai') {
+        row.cssClass = 'unit-chai';
+      }
+    }
+  }
+
   ngOnInit(): void {
     this.flexGrid.formatItem.addHandler((flex, e) => {
       console.log(flex, e);
