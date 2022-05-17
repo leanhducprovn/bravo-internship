dfad2fdf0d466ccbce55955e6f24feecc1ce3d97
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-05-17 09:22:07 +0700

    update

--- angular/src/app/wijmo/data-grid/data-grid.component.ts
+++ angular/src/app/wijmo/data-grid/data-grid.component.ts
@@ -2,15 +2,24 @@ import { Component, OnInit } from '@angular/core';
 
 import product from '../../../assets/data/product.json';
 
+import { CollectionView } from '@grapecity/wijmo';
+
 @Component({
   selector: 'app-data-grid',
   templateUrl: './data-grid.component.html',
   styleUrls: ['./data-grid.component.css'],
 })
 export class DataGridComponent implements OnInit {
-  dataTable: {}[] = product;
+  data = this.getData();
 
   constructor() {}
+
+  private getData() {
+    let data = product;
+    return new CollectionView(data, {
+      pageSize: 20,
+    });
+  }
 
   ngOnInit(): void {}
 }
