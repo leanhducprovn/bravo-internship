a78839479aeb244ce12cbbff6b51d1088b54f905
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-05-24 14:49:19 +0700

    Fix CSS
    Cập nhật CSS

--- angular/src/app/choose-time/calendar-app/calendar-app.component.ts
+++ angular/src/app/choose-time/calendar-app/calendar-app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'calendar-app',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calendar-app.component.css'],
 })
 export class CalendarAppComponent implements OnInit {
+  @Input() zLowerLabelText!: string;
+  @Input() zUpperLabelText!: string;
+
   constructor() {}
 
   ngOnInit(): void {}
