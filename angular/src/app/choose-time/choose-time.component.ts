bdee57a5e3201824d73ad299770f3cbec8f05d09
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-05-30 13:38:51 +0700

    Check branch
    Cập nhật thay đổi

--- angular/src/app/choose-time/choose-time.component.ts
+++ angular/src/app/choose-time/choose-time.component.ts
@@ -44,6 +44,7 @@ export class ChooseTimeComponent
     this.ceil = this.endDate.diff(this.startDate, 'days');
     this.slider(this.ceil, this.step);
     this.onDateEvent();
+    console.log('check branch');
   }
 
   minValue!: number;
