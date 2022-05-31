a7c25a56be9a81d3a58b0a4d17ad75ed2e44b5cb
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-05-31 08:15:36 +0700

    Thay đổi log
    Xoá một số tham số không cần thiết hiển thị

--- angular/src/app/choose-time/choose-time.component.ts
+++ angular/src/app/choose-time/choose-time.component.ts
@@ -110,11 +110,9 @@ export class ChooseTimeComponent
         .setValue(moment(this.endDate).format('YYYY-MM-DD'));
     }
     return (
-      `{type: ${
-        changeContext.pointerType === PointerType.Min ? 'min' : 'max'
-      }, ` +
-      `min: ${changeContext.value}, ` +
-      `max: ${changeContext.highValue}}`
+      `${changeContext.pointerType === PointerType.Min ? 'min' : 'max'}, ` +
+      `${changeContext.value}, ` +
+      `${changeContext.highValue}`
     );
   }
 
