c3e768d56f14facb6e405673b54d8b2a1c466eca
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-06-02 10:12:45 +0700

    Hoàn thành slider
    Hoàn thành yêu cầu về slider
    
    Co-Authored-By: Lê Anh Đức <54300988+leanhducprovn@users.noreply.github.com>

--- angular/src/app/slider/slider.component.ts
+++ angular/src/app/slider/slider.component.ts
@@ -21,71 +21,28 @@ export class SliderComponent
 
   ngAfterViewChecked(): void {}
 
-  ngDoCheck(): void {
-    this.setEvent();
-    this.slider();
-  }
+  ngDoCheck(): void {}
 
   ngOnChanges(changes: SimpleChanges): void {}
 
-  ngOnInit(): void {
-    this.slider();
-    this.onEvent();
-  }
+  ngOnInit(): void {}
 
-  start!: number;
-  end!: number;
-  options!: Options;
+  form = this.fb.group({
+    min: '',
+    start: '',
+    end: '',
+    max: '',
+  });
 
-  slider() {
-    this.start = this.setStart ? this.setStart : 0;
-    this.end = this.setEnd ? this.setEnd : 100;
-    this.options = {
-      floor: this.setMin ? this.setMin : 0,
-      ceil: this.setMax ? this.setMax : 100,
-      step: 10,
-      showTicks: true,
-    };
-  }
-
-  setStart!: number;
-  setEnd!: number;
-  setMin!: number;
-  setMax!: number;
+  min = 0;
+  start = 50;
+  end = 200;
+  max = 250;
 
   onReset() {
-    this.setStart = 0;
-    this.setEnd = 100;
-    this.setMin = 0;
-  }
-
-  onClick1() {
-    this.setStart = 20;
-    this.setEnd = 80;
-    this.setMin = 10;
-  }
-
-  onClick2() {
-    this.setStart = 30;
-    this.setEnd = 70;
-    this.setMin = 0;
-  }
-
-  event!: any;
-
-  onEvent() {
-    this.event = this.fb.group({
-      min: '',
-      start: '',
-      end: '',
-      max: '',
-    });
-  }
-
-  setEvent() {
-    this.setMin = this.event.value.min;
-    this.setStart = this.event.value.start;
-    this.setEnd = this.event.value.end;
-    this.setMax = this.event.value.max;
+    this.min = 0;
+    this.start = 50;
+    this.end = 200;
+    this.max = 250;
   }
 }
