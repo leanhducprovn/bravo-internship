73a1c3878025c596788e45c4f2937978c8f67ec8
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-06-03 10:04:44 +0700

    Cập nhật CSS ở TS
    Chẹp chẹp

--- angular/src/app/wijmo/input-date-app/input-date-app.component.ts
+++ angular/src/app/wijmo/input-date-app/input-date-app.component.ts
@@ -8,8 +8,41 @@ import { Component, OnInit } from '@angular/core';
 export class InputDateAppComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.bubble(false);
+    this.pointer('#2066a7', 18);
+  }
 
   zLowerLabelText = 'Từ ngày';
   zUpperLabelText = 'Đến ngày';
+
+  bubble(boolean: boolean) {
+    const bubble = Array.from(
+      document.getElementsByClassName(
+        'ngx-slider-bubble'
+      ) as HTMLCollectionOf<HTMLElement>
+    );
+    bubble.forEach((element) => {
+      if (boolean) {
+        element.style.display = 'block';
+      } else {
+        element.style.display = 'none';
+      }
+    });
+  }
+
+  pointer(color: string, size: number) {
+    const pointer = Array.from(
+      document.getElementsByClassName(
+        'ngx-slider-pointer'
+      ) as HTMLCollectionOf<HTMLElement>
+    );
+    pointer.forEach((element) => {
+      element.style.background = color;
+      element.style.height = size + 'px';
+      element.style.width = size + 'px';
+      element.style.border = '4px solid #fff';
+      element.style.borderRadius = '100%';
+    });
+  }
 }
