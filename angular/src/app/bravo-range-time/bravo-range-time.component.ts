21800f7e296f4354d85cd9f8a3a17cd56c802b23
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-06-06 12:39:10 +0700

    Cập nhật CSS
    Sửa lại CSS cho đẹp =))

--- angular/src/app/bravo-range-time/bravo-range-time.component.ts
+++ angular/src/app/bravo-range-time/bravo-range-time.component.ts
@@ -1,16 +1,59 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-bravo-range-time',
+  selector: 'bravo-range-time',
   templateUrl: './bravo-range-time.component.html',
   styleUrls: ['./bravo-range-time.component.css'],
 })
 export class BravoRangeTimeComponent implements OnInit {
+  month = true;
+  quarter = false;
+  year = false;
+
   constructor() {}
 
   ngOnInit(): void {}
 
-  selectRange(event: any) {
+  listMonth() {
+    let listMonth: any = [];
+    for (let i = 1; i <= 12; i++) {
+      listMonth.push(i);
+    }
+    return listMonth;
+  }
+
+  listYear() {
+    let listYear: any = [];
+    let currentYear = new Date().getFullYear();
+    for (let i = 0; i < 10; i++) {
+      currentYear++;
+      listYear.push(currentYear);
+    }
+    return listYear;
+  }
+
+  onSelectRange(event: any) {
+    let selected = event.target.value;
+    if (selected == 'Tháng') {
+      this.month = true;
+      this.quarter = false;
+      this.year = false;
+    } else if (selected == 'Quý') {
+      this.month = false;
+      this.quarter = true;
+      this.year = false;
+    } else if (selected == 'Năm') {
+      this.month = false;
+      this.quarter = false;
+      this.year = true;
+    }
+  }
+
+  onClickMonth(event: any) {
+    console.log(event.target.value);
+  }
+
+  onClickYear(event: any) {
     console.log(event.target.value);
   }
 }
